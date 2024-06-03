c985efc17cb26053810dccfbfa4cfe9f73400527
김 재웅 jaeung <jaeung@spacewalk.tech>
2024-06-03 17:45:59 +0900

    [TEST] Try more layers
    Revert "[TEST] Try more layers"
    
    This reverts commit d04e1fdb80c650059c55c7c05b6d7366ff8642d3.
    
    Update model.py

--- src/model.py
+++ src/model.py
@@ -12,20 +12,26 @@ class EncodeTensor(JSONEncoder, Dataset):
         return super(EncodeTensor, self).default(obj)
 
 
+
 class CNN(nn.Module):
     def __init__(self):
         super(CNN, self).__init__()
         self.conv1 = nn.Conv2d(1, 16, kernel_size=3, padding=1)
         self.conv2 = nn.Conv2d(16, 32, kernel_size=3, padding=1)
-        self.fc1 = nn.Linear(32 * 8 * 8, 128)
-        self.fc2 = nn.Linear(128, 2)  # FIXME: result shape fix
+        self.conv3 = nn.Conv2d(32, 64, kernel_size=3, padding=1)
+        self.fc1 = nn.Linear(64 * 4 * 4, 512)
+        self.fc2 = nn.Linear(512, 128)
+        self.fc3 = nn.Linear(128, 2)  # Output shape fix
 
     def forward(self, x):
         x = torch.relu(self.conv1(x))
         x = torch.max_pool2d(x, kernel_size=2, stride=2)
         x = torch.relu(self.conv2(x))
         x = torch.max_pool2d(x, kernel_size=2, stride=2)
-        x = x.view(-1, 32 * 8 * 8)
+        x = torch.relu(self.conv3(x))
+        x = torch.max_pool2d(x, kernel_size=2, stride=2)
+        x = x.view(-1, 64 * 4 * 4)
         x = torch.relu(self.fc1(x))
-        x = self.fc2(x)
+        x = torch.relu(self.fc2(x))
+        x = self.fc3(x)
         return x
